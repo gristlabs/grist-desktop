1c91a16c56ea0ccdac4fa6566950cf235534acf0
Spoffy <contact@spoffy.net>
2024-09-10 19:49:16 +0100

    Adds a comments explaining `electronOnly()` error

--- ext/app/client/electronOnly.ts
+++ ext/app/client/electronOnly.ts
@@ -1,5 +1,7 @@
 export function electronOnly() {
   if (window.electronAPI === undefined) {
+    // User-facing error text, preventing a `window.electronAPI is undefined` error showing to users.
+    // TODO - Find a better way to trigger the display of this.
     throw Error("Sorry, this must be done from within the app.");
   }
 }
