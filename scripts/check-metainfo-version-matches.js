26642b73ad415be5f3868af2ad8954eba953eab3
Spoffy <contact@spoffy.net>
2026-05-11 20:37:01 +0100

    Apply code review feedback

--- scripts/check-metainfo-version-matches.js
+++ scripts/check-metainfo-version-matches.js
@@ -6,9 +6,9 @@ const root = path.normalize(path.join(__dirname, '..'));
 const packageInfo = JSON.parse(readFileSync(path.join(root, 'package.json'), 'utf8'));
 const xmlParser = new XMLParser({
     ignoreAttributes: false,
-    attributeNamePrefix: "",
-    // Force "release" to always be an array
-    isArray: (name) => name === "release",
+    attributeNamePrefix: '',
+    // Force 'release' to always be an array
+    isArray: (name) => name === 'release',
 });
 const metainfo = xmlParser.parse(readFileSync(path.join(root, 'metadata', 'com.getgrist.grist.metainfo.xml'), 'utf8'));
 const metainfoReleases = metainfo.component.releases.release;
